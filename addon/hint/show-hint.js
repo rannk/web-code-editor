24583f4a22bc180f89ee3d3c16afdfe94775db31
rannk <rannk@163.com>
2018-11-01 10:17:06 +0800

    don't hint pick automatically

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -420,7 +420,7 @@
 
   var defaultOptions = {
     hint: CodeMirror.hint.auto,
-    completeSingle: true,
+    completeSingle: false,
     alignWithWord: true,
     closeCharacters: /[\s()\[\]{};:>,]/,
     closeOnUnfocus: true,
